0ae40b24ba3ad0628c65e25b3937dab085924d6e
Michael Hurley <michael.hurley@expleogroup.com>
2019-09-25 20:42:26 +0000

    degugging email set up issue

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,16 @@
 function sendMail(contactForm) {
     emailjs.send("gmail", "rosie", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_requeset": contactForm.projectsummary.value
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_requeset": contactForm.projectsummary.value
         })
         .then(
-            function (response) {
+            function(response) {
                 console.log("SUCCESS", response);
             },
-            function(error){
+            function(error) {
                 console.log("FAILED", error);
-            });
+            }
+        );
+    return false;
 }
