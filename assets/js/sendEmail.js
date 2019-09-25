5df66d658b9ce7057e686614c2a23c1754ba1c74
Michael Hurley <michael.hurley@expleogroup.com>
2019-09-25 16:48:03 +0000

    add email integration

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,14 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_requeset": contactForm.projectsummary.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function(error){
+                console.log("FAILED", error);
+            });
+}
